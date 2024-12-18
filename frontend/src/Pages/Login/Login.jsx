f3c439669f19c687e741ee59970b461ed5add9a4
DDP16 <phat16122004@gmail.com>
2024-12-18 21:46:32 +0700

    Fix Login Page

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,19 +1,30 @@
 import React, { cloneElement } from "react";
 import './Login.css';
 import Stack from '@mui/material/Stack'
-import { Container, TextField, Checkbox, FormControlLabel, Typography, Button} from "@mui/material";
+import { Container, TextField, Checkbox, FormControlLabel, Typography, Button, colors} from "@mui/material";
 import {Link, useNavigate} from 'react-router-dom'
+import Logo from '../../assets/Logo.svg'
+
 const Login = () => {
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
     return(
-        <Container maxWidth="xl" className="login">
-            <Stack className="body-login" direction="row" spacing={2} >
+        <Container style={{padding: 0}} maxWidth="xl" className="login">
+            <Stack className="body-login" direction="row" spacing={0} >
                 <Stack className="background-page">
-                    <img src="https://res.cloudinary.com/dozs7ggs4/image/upload/v1734445440/65fa9266ad67d4612ab84a9a_mWDNJVWHuNpKHlZwTTm4wAMPTVGHo1bdZR6hUmqOuQ9UEhoE-out-0_m65j3s.png"/>
+                    <img src="https://res.cloudinary.com/dsygvdfd2/image/upload/v1734527189/loginimage.png"/>
+                    <Typography className="background-title">
+                        About us
+                    </Typography>
+                    <Typography className="background-content">
+                        Our warehouse management team excels in efficiency, precision, and reliability. We are committed to delivering top-tier management practices that ensure seamless operations, from inventory tracking to order fulfillment. With a focus on optimizing space, minimizing errors, and streamlining processes, we consistently achieve fast and accurate deliveries.
+                    </Typography>
                 </Stack>
                 {/* justifyContent="space-between" */}
-                <Stack className="information-page">
+                <Stack justifyContent="center" className="information-page">
                     <Stack className="title" alignItems={"center"}>
-                        <img src="https://res.cloudinary.com/dozs7ggs4/image/upload/v1734362317/WarehouseManagement-3_kibnmb.png"/>
+                        <img src={Logo}/>
                         <Typography 
                             className="title-login" 
                             variant="h5" 
@@ -34,12 +45,12 @@ const Login = () => {
                         id="outlined-password-input"
                         className="infor-pass"
                         label="Password"
-                        type="password"
+                        type={showPassword ? 'text' : 'password'}
                         autoComplete="current-password"
                         />
 
                     <Stack direction="row" alignItems={"space-between"} >
-                        <FormControlLabel control={<Checkbox defaultChecked />} label="Hiện mật khẩu" />
+                        <FormControlLabel control={<Checkbox style ={{color: "#F25D07",}} onClick={handleClickShowPassword}/>} label="Hiện mật khẩu" />
                     </Stack>
 
                     <Stack direction="column" alignItems={"center"}>
