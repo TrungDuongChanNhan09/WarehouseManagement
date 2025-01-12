8370b471869b991daa6414c1c77a878c30cce888
Vminhkiet <minhkietvo6@gmail.com>
2025-01-12 19:50:03 +0700

    update exists shelf

--- backend/src/main/java/com/example/backend/controller/ShelfController.java
+++ backend/src/main/java/com/example/backend/controller/ShelfController.java
@@ -1,7 +1,9 @@
 package com.example.backend.controller;
+
 import com.example.backend.model.Inventory;
 import com.example.backend.model.Shelf;
 import com.example.backend.model.User;
+import com.example.backend.respone.ShelfEmpty;
 import com.example.backend.service.ShelfService;
 import com.example.backend.service.UserService;
 
@@ -13,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-
 @RestController
 @RequestMapping("/api/shelf")
 
@@ -24,60 +25,77 @@ public class ShelfController {
     private UserService userService;
 
     @GetMapping("/inventory/{inventoryId}")
-    public ResponseEntity<List<Shelf>> getShelvesByInventory(@PathVariable String inventoryId, @RequestHeader("Authorization") String jwt) throws Exception {
+    public ResponseEntity<List<Shelf>> getShelvesByInventory(@PathVariable String inventoryId,
+            @RequestHeader("Authorization") String jwt) throws Exception {
         User user = userService.findUserByJwtToken(jwt);
         List<Shelf> shelves = shelfService.getShelvesByInventory(inventoryId);
         return new ResponseEntity<>(shelves, HttpStatus.OK);
     }
 
     @PostMapping("")
-    public ResponseEntity<Shelf> addShelf(@RequestBody Shelf shelf, @RequestHeader("Authorization") String jwt) throws Exception {
+    public ResponseEntity<Shelf> addShelf(@RequestBody Shelf shelf, @RequestHeader("Authorization") String jwt)
+            throws Exception {
         User user = userService.findUserByJwtToken(jwt);
         Shelf createdShelf = shelfService.addShelf(shelf);
         return new ResponseEntity<>(createdShelf, HttpStatus.CREATED);
     }
 
     @PutMapping("/{shelfId}")
-    public ResponseEntity<Shelf> updateShelf(@PathVariable String shelfId, @RequestBody Shelf updatedShelf, @RequestHeader("Authorization") String jwt) throws Exception {
-        
+    public ResponseEntity<Shelf> updateShelf(@PathVariable String shelfId, @RequestBody Shelf updatedShelf,
+            @RequestHeader("Authorization") String jwt) throws Exception {
+
         User user = userService.findUserByJwtToken(jwt);
         Shelf updated = shelfService.updateShelf(shelfId, updatedShelf);
         return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 
     @DeleteMapping("/{shelfId}")
-    public ResponseEntity<Void> deleteShelf(@PathVariable String shelfId, @RequestHeader("Authorization") String jwt) throws Exception{
-        
+    public ResponseEntity<Void> deleteShelf(@PathVariable String shelfId, @RequestHeader("Authorization") String jwt)
+            throws Exception {
+
         User user = userService.findUserByJwtToken(jwt);
         shelfService.deleteShelf(shelfId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/{shelfId}")
-    public ResponseEntity<Shelf> getShelfById(@PathVariable String shelfId, @RequestHeader("Authorization") String jwt) throws Exception{
+    public ResponseEntity<Shelf> getShelfById(@PathVariable String shelfId, @RequestHeader("Authorization") String jwt)
+            throws Exception {
         User user = userService.findUserByJwtToken(jwt);
 
         Optional<Shelf> shelf = shelfService.getShelfById(shelfId);
         return shelf.map(s -> new ResponseEntity<>(s, HttpStatus.OK))
-                    .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
+
     @GetMapping("/searchByName/{keyword}")
-    public ResponseEntity<List<Shelf>> searchInventoriesByName(@PathVariable String keyword, @RequestHeader("Authorization") String jwt) throws Exception {
+    public ResponseEntity<List<Shelf>> searchInventoriesByName(@PathVariable String keyword,
+            @RequestHeader("Authorization") String jwt) throws Exception {
         User user = userService.findUserByJwtToken(jwt);
         List<Shelf> inventories = shelfService.searchShelfByCode(keyword);
         return new ResponseEntity<>(inventories, HttpStatus.OK);
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<Shelf>> getAllShelves(@RequestHeader("Authorization") String jwt) throws Exception{
-        
+    public ResponseEntity<List<Shelf>> getAllShelves(@RequestHeader("Authorization") String jwt) throws Exception {
+
         User user = userService.findUserByJwtToken(jwt);
         List<Shelf> shelves = shelfService.getAllShelves();
         return new ResponseEntity<>(shelves, HttpStatus.OK);
     }
 
+    @GetMapping("/exists/{inventoryid}")
+    public ResponseEntity<ShelfEmpty> getAllShelfEmpty(@PathVariable String inventoryid,
+            @RequestHeader("Authorization") String jwt) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+        ShelfEmpty shelfEmpty = shelfService.getPositionShelfEmpty(inventoryid);
+        return new ResponseEntity<>(shelfEmpty, HttpStatus.OK);
+    }
+
     @GetMapping("/allShelfCode/{productName}")
-    public ResponseEntity<List<String>> getAllShelfCode(@RequestHeader("Authorization") String jwt, @PathVariable String productName) throws Exception{
+    public ResponseEntity<List<String>> getAllShelfCode(@RequestHeader("Authorization") String jwt,
+            @PathVariable String productName) throws Exception {
 
         User user = userService.findUserByJwtToken(jwt);
         return new ResponseEntity<>(shelfService.getShelfContainProduct(productName), HttpStatus.OK);
--- backend/src/main/java/com/example/backend/service/ShelfService.java
+++ backend/src/main/java/com/example/backend/service/ShelfService.java
@@ -5,19 +5,28 @@ import java.util.Optional;
 
 import com.example.backend.model.Inventory;
 import com.example.backend.model.Shelf;
+import com.example.backend.respone.ShelfEmpty;
 
 public interface ShelfService {
     List<Shelf> getShelvesByInventory(String inventoryId);
+
     Shelf addShelf(Shelf shelf) throws Exception;
+
     Shelf updateShelf(String shelfId, Shelf updatedShelf) throws Exception;
+
     void deleteShelf(String shelfId);
 
     void updatequantityShelf(String shelfId, int quantity) throws Exception;
 
     List<Shelf> searchShelfByCode(String keyword);
+
     Optional<Shelf> getShelfById(String shelfId);
+
     boolean shelfExists(String shelfId);
+
     List<Shelf> getAllShelves();
 
     List<String> getShelfContainProduct(String productId);
+
+    ShelfEmpty getPositionShelfEmpty(String Inventoryid);
 }
